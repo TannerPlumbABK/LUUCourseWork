480cf78c6494f00acb28f68753f45e760608dc9a
TannerPlumbABK <tanner.plumb@activision.com>
2022-07-25 08:37:10 -0700

    - Updated code style for pointers and references challenge

--- 02_01_PointersAndReferences/02_01_PointersAndReferences.cpp
+++ 02_01_PointersAndReferences/02_01_PointersAndReferences.cpp
@@ -1,46 +1,53 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+void GetInput(int& a, int& b);
+void DisplayInput(int* a, int* b);
+void CinError(string error);
+
 int main()
 {
-	int a;
-	int b;
-
-	cout << "Enter two numbers" << endl;
-
-	cout << "Number 1: ";
-	for (;;)
-	{
-		if (cin >> a) { break; }
-		else
-		{
-			cout << "Please enter a valid whole number." << endl;
-			cout << "Number 1: ";
-			cin.clear();
-			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		}
-	}
-
-	cout << "Number 2: ";
-	for (;;)
-	{
-		if (cin >> b) { break; }
-		else
-		{
-			cout << "Please enter a valid whole number." << endl;
-			cout << "Number 2: ";
-			cin.clear();
-			cin.ignore(numeric_limits<streamsize>::max(), '\n');
-		}
-	}
-
-	cout << endl;
+	int a, b;
+	GetInput(a, b);
 
 	int* ptrA = &a;
 	int* ptrB = &b;
+	DisplayInput(ptrA, ptrB);
+}
 
+void CinError(string error)
+{
+	cout << error;
+	cin.clear();
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
+
+void GetInput(int& a, int& b)
+{
+	cout << "Enter two numbers" << endl;
+
+	cout << "Number 1: ";
+	while (true)
+	{
+		if (cin >> a) break;
+		else CinError("Please enter a valid whole number.\nNumber 1: ");
+	}
+
+	cout << "Number 2: ";
+	while (true)
+	{
+		if (cin >> b) break;
+		else CinError("Please enter a valid whole number.\nNumber 2: ");
+	}
+
+	cout << endl;
+}
+
+void DisplayInput(int* a, int* b)
+{
 	cout << "You Entered:" << endl;
-	cout << "Number 1: " << *ptrA << endl;
-	cout << "Number 2: " << *ptrB << endl;
+	cout << "Number 1: " << *a << " (" << a << ")" << endl;
+	cout << "Number 2: " << *b << " (" << b << ")" << endl;
 }
