aba688204b0b9008366bd2ba5d823337f8eb8e72
TannerPlumbABK <tanner.plumb@activision.com>
2022-08-15 09:34:56 -0700

    Initial Maze Game Commit

---
+++ UE_MazeGame/Source/UE_MazeGame/UE_MazeGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE_MazeGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE_MazeGame, "UE_MazeGame" );
---
+++ UE_MazeGame/Source/UE_MazeGame/UE_MazeGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE_MazeGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_MAZEGAME_API AUE_MazeGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
