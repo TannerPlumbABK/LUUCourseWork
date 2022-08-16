edeeaff5feaa2a8a6d2d3757db23891ca466a439
TannerPlumbABK <tanner.plumb@activision.com>
2022-08-16 13:41:30 -0700

    - Started map creation - Created Spike trap interaction

---
+++ UE_MazeGame/Source/UE_MazeGame/Private/TrapInteractionComponent.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TrapInteractionComponent.h"
+#include "Engine/TriggerBox.h"
+
+// Sets default values for this component's properties
+UTrapInteractionComponent::UTrapInteractionComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UTrapInteractionComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UTrapInteractionComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (TriggerBox && GetWorld() && GetWorld()->GetFirstLocalPlayerFromController())
+	{
+		APawn* PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+		if (PlayerPawn && TriggerBox->IsOverlappingActor(PlayerPawn))
+		{
+			UE_LOG(LogTemp, Log, TEXT("SPIKES"));
+		}
+	}
+}
+
--- UE_MazeGame/Source/UE_MazeGame/Public/TrapInteractionComponent.h
+++ UE_MazeGame/Source/UE_MazeGame/Public/TrapInteractionComponent.h
@@ -4,43 +4,25 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
-#include "Curves/CurveFloat.h"
-#include "DoorInteractionComponent.generated.h"
+#include "TrapInteractionComponent.generated.h"
 
 class ATriggerBox;
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class UE_MAZEGAME_API UDoorInteractionComponent : public UActorComponent
+class UE_MAZEGAME_API UTrapInteractionComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:	
 	// Sets default values for this component's properties
-	UDoorInteractionComponent();
+	UTrapInteractionComponent();
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
 	UPROPERTY(EditAnywhere)
-		FRotator DesiredRotation = FRotator::ZeroRotator;
-
-	FRotator StartRotation = FRotator::ZeroRotator;
-	FRotator FinalRotation = FRotator::ZeroRotator;
-
-	UPROPERTY(EditAnywhere)
-		float TimeToRotate = 1.0f;
-
-	float CurrentRotationTime = 0.0f;
-
-	UPROPERTY(EditAnywhere)
 		ATriggerBox* TriggerBox;
-
-	UPROPERTY(EditAnywhere)
-		FRuntimeFloatCurve OpenCurve;
-
-	UPROPERTY(EditAnywhere)
-		bool AutoClose = false;
 
 public:	
 	// Called every frame
